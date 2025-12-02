4b07ee4082f41b5bef47d835d6c0f83d20b58126
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2025-12-02 19:23:13 +0500

    learn how to take input and display output

--- Lecture2/hello.cpp
+++ Lecture2/hello.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main(){
 
 
-    cout << "Hello dsa 2026";
+    cout << "Hello dsa 2026" << endl;
+    cout << "Tasbiha" << "\n" << "Anwar Adil" << endl;
+    cout << 2 + 2 << endl;
     return 0;
 }
 
---
+++ Lecture2/variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    cout << "Age is: " << age << endl;
+
+    return 0;
+}
