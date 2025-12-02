fffee09f683c921d80492c98196087ff3d8ee12c
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2025-12-02 22:27:51 +0500

    Problem Solved: Sum of two numbers

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+// Program to add two numbers
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num1, num2;
+
+    cout << "Enter number 1: " << endl;
+    cin >> num1;
+
+    cout << "Enter number 2: " << endl;
+    cin >> num2;
+
+    cout << "Sum: " << (num1 + num2) << endl;
+
+    return 0;
+}
