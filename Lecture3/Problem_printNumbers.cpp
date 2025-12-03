e23a0df894e1a0f12d6f44bb32c7c4944987607a
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2025-12-03 18:53:28 +0500

    Problem Solved: Print Numbers form 1-5 using while Loop

---
+++ Lecture3/Problem_printNumbers.cpp
@@ -0,0 +1,13 @@
+//Print Numbers form 1-5
+
+#include <iostream>
+using namespace std;
+int main(){
+    int count = 1;
+
+    while(count<=5){
+        cout << count << endl;
+        count++;
+    }
+    return 0;
+}
